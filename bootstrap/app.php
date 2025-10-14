10183d3f9aa84f21c82892c0ef26ce273048a566
Moskov-1 <wwwronyraihan123@gmail.com>
2025-10-14 06:12:39 +0600

    update: auth setup added from v2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\admin;
+use App\Http\Middleware\JWTMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin.auth' => admin::class,
+            'jwt.verify' => JWTMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
